dc3b1cb5c5fcfbd5742fe11a16de43b0e3b0e49e
rowon3375 <ilove4622@gmail.com>
2022-10-22 22:36:08 +0900

    10/7(金)〜10/22(土) PHP 1. PHP basic study 2. file upload(37.php & 37_1.html)

--- PHP basic/basic_1/37.php
+++ PHP basic/basic_1/37.php
@@ -5,8 +5,11 @@
   </head>
   <body>
     <?php
-      ini set("display_errors", "1");
+#php설정을 runtime(php가 실행되는 시점)으로 지정하는 것
+      ini_set("display_errors", "1");
+#php애플리케이션이 업로드된 파일을 임시 저장소에서 파일 디렉토리로 이동하기 위해서 사용된 경로=>컴퓨터상의 경로(드라이브)
       $uploaddir = "./Application/mampstack-8.1.11-0/apache2/htdocs";
+#basename=>파일에 실제이름이 정확하게 해당 파일의 이름을 의미하도록 한다.(보안 관련)
       $uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
 
       echo "<pre>";
@@ -14,13 +17,14 @@
       if(move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)){
         echo "파일이 유효하고 성공적으로 업로드 되었습니다 \n";
       } else{
-        print "파일이 업도르 공격의 가능성이 있습니다. \n"
+        print "파일이 업도르 공격의 가능성이 있습니다. \n";
       }
       echo "자세한 디버깅 정보입니다 : ";
       print_r($_FILES);
       print "</pre>"
 
      ?>
+<!-- 도메인, url 경로 -->
      <img src="file/<?=$_FILES['userfile']['name']?>">
   </body>
 </html>
