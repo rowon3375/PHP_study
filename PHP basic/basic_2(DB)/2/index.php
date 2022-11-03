8c7284e15170da4dc1c35886e0694c54d284308d
rowon3375 <ilove4622@gmail.com>
2022-11-03 16:18:09 +0900

    10/7(金)〜11/3(水) PHP 1. PHP basic study 2. file upload(37.php & 37_1.html) 3. imgae input text(38.php & 38_1.html) 4. watermark(39.php) 5. 정규 표현식(40.php~48.php) 6. DataBase insert, select, update, delete(input.php~list.php) 7. DataBsse error 8. Cookie&Session 9. join&login

---
+++ PHP basic/basic_2(DB)/2/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>index</title>
+  </head>
+  <body>
+    <table width="800" height="600" border="1">
+      <tr>
+        <td colspan="2">상단영역</td>
+      </tr>
+      <tr>
+        <td width="600">중간</td>
+        <td valign="top" style="text-align:center">
+          <!-- 로그인 -->
+          <?php require("loginForm.php"); ?>
+        </td>
+      </tr>
+      <tr>
+        <td colspan="2">하단</td>
+      </tr>
+    </table>
+  </body>
+</html>
